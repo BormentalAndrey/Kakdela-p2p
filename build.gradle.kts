66d50c06d6faac9892923904c963449c6afa2210
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-06 01:01:14 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,14 @@
+// Top-level build file: build.gradle.kts (Project level)
 plugins {
-    id("com.android.application") version "8.7.0" apply false
+    id("com.android.application") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("kotlin-kapt") apply false
 }
 
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+// Репозитории для всех модулей
+allprojects {
     repositories {
         google()
         mavenCentral()
     }
 }
-
-rootProject.name = "Kakdela-p2p"
-include(":app")
