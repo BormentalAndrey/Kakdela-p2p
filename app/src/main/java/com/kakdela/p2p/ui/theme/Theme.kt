9b0ae8f12409b00fd344a880290fe4c7bf2dfb6e
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-05 16:41:37 +0300

    Update Theme.kt

--- app/src/main/java/com/kakdela/p2p/ui/theme/Theme.kt
+++ app/src/main/java/com/kakdela/p2p/ui/theme/Theme.kt
@@ -1,12 +1,17 @@
 package com.kakdela.p2p.ui.theme
 
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
-private val DarkColors = darkColorScheme()
+private val LightColorScheme = lightColorScheme(
+    primary = androidx.compose.ui.graphics.Color(0xFF6200EE)
+)
 
 @Composable
-fun KakDelaTheme(content: @Composable () -> Unit) {
-    MaterialTheme(colorScheme = DarkColors, content = content)
+fun KakdelaP2pTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colorScheme = LightColorScheme,
+        content = content
+    )
 }
