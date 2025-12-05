18d45cacd545f2f0fdf8b4e4bcb62bd81b83f44b
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-06 01:47:59 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
-// Top-level build file: build.gradle.kts (Project level)
+// build.gradle.kts (Project level — корень проекта)
 plugins {
-    id("com.android.application") version "8.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.android.application") version "8.4.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("kotlin-kapt") apply false
 }
 
-// Репозитории для всех модулей
 allprojects {
     repositories {
         google()
