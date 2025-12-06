608df8efb4a686cd4f0c94024ef073e44958cc10
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-06 08:12:21 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("org.jetbrains.kotlin.jvm") version = "2.1.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.1.0" apply false
 
 allprojects {
     repositories {
