21bd8ee7985ca4061204a2de433f1fa5fb5495c8
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-07 08:16:01 +0300

    Update MainActivity.kt

--- app/src/main/java/com/kakdela/p2p/MainActivity.kt
+++ app/src/main/java/com/kakdela/p2p/MainActivity.kt
@@ -7,18 +7,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.kakdela.p2p.ui.screens.ChatScreen
 import com.kakdela.p2p.ui.theme.KakdelaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             KakdelaTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    NavGraph() // ← здесь твой главный экран
+                    // Главный экран — твой чат!
+                    ChatScreen()
                 }
             }
         }
