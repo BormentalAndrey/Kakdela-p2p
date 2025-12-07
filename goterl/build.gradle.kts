6e315b882931df4e8ee72d6cf714a3477020dd69
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-07 23:50:07 +0300

    Update build.gradle.kts

--- goterl/build.gradle.kts
+++ goterl/build.gradle.kts
@@ -6,12 +6,23 @@ plugins {
 android {
     namespace = "com.kakdela.goterl"
     compileSdk = 35
-    defaultConfig { 
-        minSdk = 24 
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    // !!! ВЫРАВНИВАЕМ JAVA И KOTLIN !!!
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
     }
 }
 
 dependencies {
-    // Lazysodium уже включает JNA. Ничего больше добавлять не нужно.
+    // Lazysodium already includes JNA — DO NOT ADD JNA MANUALLY
     implementation("com.goterl:lazysodium-android:5.1.0")
 }
