75b9846e221736d41d6acdcd7f0d2bcd9289a2a7
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-07 07:17:53 +0300

    Create Color.kt

---
+++ app/src/main/java/com/kakdela/p2p/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.kakdela.p2p.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFE8DEF8)
+val Purple500 = Color(0xFF9C27B0)
+val Purple700 = Color(0xFF7B1FA2)
+val Teal200 = Color(0xFF80CBC4)
+
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
