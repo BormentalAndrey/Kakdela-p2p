996432c2258b9b82070c4da94a5d5cd388bded3f
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-07 14:06:25 +0300

    Update build.gradle.kts

--- goterl/build.gradle.kts
+++ goterl/build.gradle.kts
@@ -6,17 +6,10 @@ plugins {
 android {
     namespace = "com.kakdela.goterl"
     compileSdk = 35
-
-    defaultConfig {
-        minSdk = 24
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+    defaultConfig { minSdk = 24 }
 }
 
 dependencies {
     implementation("com.goterl:lazysodium-android:5.1.0")
+    implementation("net.java.dev.jna:jna:5.14.0@aar")
 }
