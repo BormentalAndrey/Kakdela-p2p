c3b52eda10e59c9c5ffa3b3f30533618f7045187
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-07 14:04:42 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,4 @@
-// Top-level build file
 plugins {
     id("com.android.application") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.25" apply false
-    // kapt не нужен на уровне root
 }
-
-// В Gradle 8.7 с FAIL_ON_PROJECT_REPOS блок repositories не нужен и его убираем
