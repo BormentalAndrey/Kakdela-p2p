bd87dbb6d0a1445309774968d590878bc6e9fbc4
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-07 15:46:41 +0300

    Update build.gradle.kts

--- goterl/build.gradle.kts
+++ goterl/build.gradle.kts
@@ -11,18 +11,15 @@ android {
         minSdk = 24
     }
 
-    // ✔ Выравниваем Java для Gradle/Javac
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    // ✔ Настраиваем Kotlin
     kotlinOptions {
         jvmTarget = "17"
     }
 
-    // ✔ Принудительный JVM Toolchain (решает твою ошибку)
     kotlin {
         jvmToolchain(17)
     }
@@ -30,5 +27,8 @@ android {
 
 dependencies {
     implementation("com.goterl:lazysodium-android:5.1.0")
-    implementation("net.java.dev.jna:jna:5.14.0@aar")
+
+    implementation("net.java.dev.jna:jna:5.14.0") {
+        exclude(group = "net.java.dev.jna", module = "jna-android")
+    }
 }
