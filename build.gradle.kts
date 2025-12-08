9fa75fa9db8dc035b4913ad34edac5af3a695a11
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-08 20:42:15 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,5 @@
-// Top-level build file
 plugins {
     id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    // kapt больше не нужен — ты ничего не обрабатываешь аннотациями
-    // если вдруг потом понадобится — добавим KSP
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+    id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false  // ← ЭТО ВАЖНО
 }
