1d1a2408960332dd1cf281b6552a7d4faebaef92
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-08 20:58:47 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,8 @@
 plugins {
     id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false // ЭТА СТРОКА РЕШАЕТ ВСЁ
+    id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false
+    id("org.jetbrains.compose") version "1.6.11" apply false // ЭТА СТРОКА РЕШАЕТ ВСЁ
 }
 
 tasks.register<Delete>("clean") {
