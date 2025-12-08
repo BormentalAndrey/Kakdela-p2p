2aa08c61223a45b9948c9da264f88615430fd1cf
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-08 21:07:57 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp") // правильный и короткий вариант, работает точно так же
+    id("org.jetbrains.compose")     // ЭТА СТРОКА — ВСЁ, ЧТО НУЖНО
+    id("com.google.devtools.ksp")
 }
 
 android {
