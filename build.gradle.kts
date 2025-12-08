78bf127645dfe43ebe4d1a472f28b13e9b019aed
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-08 20:45:40 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
+// Top-level build file
 plugins {
     id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false  // ← ЭТО ВАЖНО
+    id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false // ЭТА СТРОКА РЕШАЕТ ВСЁ
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
