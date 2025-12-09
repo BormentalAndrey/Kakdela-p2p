7e1f64cf8f62f98198d75b67d02394fc8cddaf58
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-09 10:50:28 +0300

    Update Color.kt

--- app/src/main/java/com/kakdela/p2p/ui/theme/Color.kt
+++ app/src/main/java/com/kakdela/p2p/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package com.kakdela.p2p.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFE8DEF8)
-val Purple500 = Color(0xFF9C27B0)
-val Purple700 = Color(0xFF7B1FA2)
-val Teal200 = Color(0xFF80CBC4)
+val NeonCyan = Color(0xFF00FFFF)
+val NeonPink = Color(0xFFFF00FF)
+val NeonPurple = Color(0xFF8A2BE2)
+val NeonBlue = Color(0xFF00D4FF)
+val NeonGreen = Color(0xFF39FF14)
 
-val Black = Color(0xFF000000)
-val White = Color(0xFFFFFFFF)
+val BackgroundDark = Color(0xFF0A0A0A)
+val SurfaceDark = Color(0xFF121212)
+val OnBackground = Color(0xFFE0E0E0)
+val OnSurface = Color(0xFFFFFFFF)
