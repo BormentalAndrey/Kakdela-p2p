7e0344d91e80ab87c87e209f5eab2d364c6ad691
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-09 04:45:34 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,3 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -16,4 +8,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "Kakdela-p2p"
 include(":app")
-include(":goterl")  // ← ОБЯЗАТЕЛЬНО!
