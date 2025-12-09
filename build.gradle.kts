c5ec29ca2b94ce942a3a2720a1e5b1e62716483b
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-09 17:22:02 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,6 @@
-// Top-level build file
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
 }
-
-true // Needed for version catalog to work in precompiled script plugins
