bd5c633d0020a8112bad9ab8a1cbb2cbc3e65750
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-09 04:44:46 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,9 @@
-// Top of file: plugins block (update versions as needed)
+// Top-level build file
 plugins {
-    id("com.android.application") version "8.5.0" apply false  // Use latest AGP compatible with Gradle 8.7
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false  // Match Kotlin for KSP
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false  // NEW: Compose Compiler Plugin
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ksp) apply false
 }
 
-// Fix deprecation: Replace any `buildDir` usage, e.g.:
-// Instead of: val customDir = buildDir.resolve("custom")
-// Use: val customDir = layout.buildDirectory.dir("custom").get().asFile
+true // Needed for version catalog to work in precompiled script plugins
