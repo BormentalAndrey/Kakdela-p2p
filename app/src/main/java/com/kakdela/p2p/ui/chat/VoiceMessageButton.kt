7ddd0e4b1e5eba0112882f4fdded1b28d1b70d67
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-10 18:14:18 +0300

    Update VoiceMessageButton.kt

--- app/src/main/java/com/kakdela/p2p/ui/chat/VoiceMessageButton.kt
+++ app/src/main/java/com/kakdela/p2p/ui/chat/VoiceMessageButton.kt
@@ -10,7 +10,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.*
 @Composable
 fun VoiceMessageButton(
     isRecording: Boolean,
