d3533b13bef406de2c2dba78ce9744572acfc59d
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-10 15:33:46 +0300

    Update MainActivity.kt

--- app/src/main/java/com/kakdela/p2p/MainActivity.kt
+++ app/src/main/java/com/kakdela/p2p/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.kakdela.p2p.ui.screens.ChatScreen
-import com.kakdela.p2p.ui.theme.KakdelaP2PTheme
+import com.kakdela.p2p.ui.theme.KakdelaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            KakdelaP2PTheme {
+            KakdelaTheme {
                 ChatScreen()
             }
         }
