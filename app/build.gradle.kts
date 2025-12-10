1254dfdab26a404910cbb3a8700fe8e6b8b0aee2
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-10 13:17:40 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,6 @@ dependencies {
     implementation(libs.compose.ui.graphics)
     implementation(libs.compose.ui.tooling.preview)
     implementation(libs.compose.material3)
-    implementation(libs.compose.material.icons.extended)
 
     implementation(libs.androidx.activity.compose)
 
