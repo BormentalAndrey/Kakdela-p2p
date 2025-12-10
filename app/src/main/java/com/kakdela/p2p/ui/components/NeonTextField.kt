d2d543115e44a8acbc04ab4b24885ce7c76b52cb
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-10 15:23:20 +0300

    Update NeonTextField.kt

--- app/src/main/java/com/kakdela/p2p/ui/components/NeonTextField.kt
+++ app/src/main/java/com/kakdela/p2p/ui/components/NeonTextField.kt
@@ -12,7 +12,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-
+import androidx.compose.material3.icons.Icons
+import androidx.compose.material3.icons.filled.Send
 @Composable
 fun NeonTextField(
     value: String,
