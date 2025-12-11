cac2fa664328e11a8b058a2407ddf97a74e10815
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-11 20:32:08 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,19 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.ksp) apply false  // ← Это строка 1: теперь версия 2.2.20-2.0.4 (существует!)
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
