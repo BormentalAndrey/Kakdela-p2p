74b5c76696b4fd68be6c82dcba304c9c2d1f9621
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-12 21:08:59 +0300

    Update ChatMessageDao.kt

--- app/src/main/java/com/kakdela/p2p/db/ChatMessageDao.kt
+++ app/src/main/java/com/kakdela/p2p/db/ChatMessageDao.kt
@@ -2,17 +2,19 @@ package com.kakdela.p2p.db
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ChatMessageDao {
-    @Insert
-    suspend fun insert(message: ChatMessage)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(msg: ChatMessage)
 
     @Query("SELECT * FROM ChatMessage WHERE chatId = :chatId ORDER BY timestamp ASC")
     fun observeChat(chatId: String): Flow<List<ChatMessage>>
 
-    @Query("SELECT * FROM ChatMessage ORDER BY timestamp DESC")
+    @Query("SELECT * FROM ChatMessage ORDER BY timestamp ASC")
     fun observeAll(): Flow<List<ChatMessage>>
 }
