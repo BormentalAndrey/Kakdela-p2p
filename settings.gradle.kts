69cfbfc5187bfee82c065b9845e9e62b8ee19a34
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-12 08:42:48 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
         id("com.android.application") version "8.4.0"
         id("org.jetbrains.kotlin.android") version "1.9.22"
         id("org.jetbrains.kotlin.plugin.compose") version "1.9.22"
-        id("com.google.devtools.ksp") version "1.9.10-1.0.13"
+        id("com.google.devtools.ksp") version "1.9.22-1.0.11"
     }
 }
 
