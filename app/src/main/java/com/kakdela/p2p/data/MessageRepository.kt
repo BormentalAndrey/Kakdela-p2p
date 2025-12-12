c20dd36cc2624cd3e2b81234f30290c588a38c9e
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-12 16:28:42 +0300

    Create MessageRepository.kt

---
+++ app/src/main/java/com/kakdela/p2p/data/MessageRepository.kt
@@ -0,0 +1,15 @@
+package com.kakdela.p2p.data
+
+import com.kakdela.p2p.db.ChatMessage
+import com.kakdela.p2p.db.ChatMessageDao
+import kotlinx.coroutines.flow.Flow
+
+class MessageRepository(private val dao: ChatMessageDao) {
+    suspend fun sendMessage(chatId: String, author: String, text: String) {
+        val msg = ChatMessage(chatId = chatId, author = author, text = text)
+        dao.insert(msg)
+    }
+
+    fun observeChat(chatId: String): Flow<List<ChatMessage>> = dao.observeChat(chatId)
+    fun observeAll(): Flow<List<ChatMessage>> = dao.observeAll()
+}
