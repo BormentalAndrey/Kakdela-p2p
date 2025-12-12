67c2368685536308fcb98fcf03ca5228c16c4f04
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-12 16:27:10 +0300

    Update ChatMessageDao.kt

--- app/src/main/java/com/kakdela/p2p/db/ChatMessageDao.kt
+++ app/src/main/java/com/kakdela/p2p/db/ChatMessageDao.kt
@@ -7,10 +7,12 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ChatMessageDao {
-
     @Insert
     suspend fun insert(message: ChatMessage)
 
-    @Query("SELECT * FROM ChatMessage WHERE senderId = :user OR receiverId = :user ORDER BY timestamp ASC")
-    fun getMessages(user: String): Flow<List<ChatMessage>>
+    @Query("SELECT * FROM ChatMessage WHERE chatId = :chatId ORDER BY timestamp ASC")
+    fun observeChat(chatId: String): Flow<List<ChatMessage>>
+
+    @Query("SELECT * FROM ChatMessage ORDER BY timestamp DESC")
+    fun observeAll(): Flow<List<ChatMessage>>
 }
