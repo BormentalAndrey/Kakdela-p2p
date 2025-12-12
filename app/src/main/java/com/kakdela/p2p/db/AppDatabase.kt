204e942c6151831ad25a63fe6a8ec71e525cbda8
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-12 20:58:40 +0300

    Update AppDatabase.kt

--- app/src/main/java/com/kakdela/p2p/db/AppDatabase.kt
+++ app/src/main/java/com/kakdela/p2p/db/AppDatabase.kt
@@ -5,20 +5,25 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [ChatMessage::class], version = 1, exportSchema = false)
+@Database(
+    entities = [ChatMessage::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
+
     abstract fun chatMessageDao(): ChatMessageDao
 
     companion object {
         @Volatile private var INSTANCE: AppDatabase? = null
 
-        fun getInstance(context: Context): AppDatabase =
+        fun getInstance(ctx: Context): AppDatabase =
             INSTANCE ?: synchronized(this) {
                 INSTANCE ?: Room.databaseBuilder(
-                    context.applicationContext,
+                    ctx.applicationContext,
                     AppDatabase::class.java,
                     "kakdela_db"
-                ).fallbackToDestructiveMigration().build().also { INSTANCE = it }
+                ).build().also { INSTANCE = it }
             }
     }
 }
