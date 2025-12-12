6571cc7421616663c086e71af04922be27bd2cdd
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-12 16:16:58 +0300

    Update AppDatabase.kt

--- app/src/main/java/com/kakdela/p2p/db/AppDatabase.kt
+++ app/src/main/java/com/kakdela/p2p/db/AppDatabase.kt
@@ -3,7 +3,13 @@ package com.kakdela.p2p.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [ChatMessage::class], version = 1, exportSchema = false)
+@Database(
+    entities = [
+        ChatMessage::class
+    ],
+    version = 1,
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun messageDao(): MessageDao
+    abstract fun chatMessageDao(): ChatMessageDao
 }
