712ca3b02b5366e9397ea1e5a248f0232b952e30
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-13 10:44:23 +0300

    Update NeonTextField.kt

--- app/src/main/java/com/kakdela/p2p/ui/components/NeonTextField.kt
+++ app/src/main/java/com/kakdela/p2p/ui/components/NeonTextField.kt
@@ -1,8 +1,19 @@
 package com.kakdela.p2p.ui.components
 
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun NeonTextField(value: String, onValueChange: (String) -> Unit) {
-    // Неоновое поле ввода
+fun NeonTextField(value: String, onValueChange: (String) -> Unit, modifier: Modifier = Modifier) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        textStyle = TextStyle(color = Color(0xFF00FFF0), fontWeight = FontWeight.Bold, fontSize = 16.sp),
+        modifier = modifier
+    )
 }
