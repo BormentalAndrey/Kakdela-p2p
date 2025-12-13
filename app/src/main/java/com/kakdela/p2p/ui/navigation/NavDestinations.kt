78c8a761fe929ea5adc04ed539083aef4d32eb5e
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-13 10:02:06 +0300

    Update NavDestinations.kt

--- app/src/main/java/com/kakdela/p2p/ui/navigation/NavDestinations.kt
+++ app/src/main/java/com/kakdela/p2p/ui/navigation/NavDestinations.kt
@@ -2,7 +2,9 @@ package com.kakdela.p2p.ui.navigation
 
 sealed class Screen(val route: String) {
     object Contacts : Screen("contacts")
-    object Chat : Screen("chat/{chatId}") {
-        fun route(chatId: String) = "chat/$chatId"
+    object Chat : Screen("chat/{peerId}") {
+        fun route(peerId: String) = "chat/$peerId"
     }
+    object MyQr : Screen("myQr")
+    object QrScanner : Screen("qrScanner")
 }
