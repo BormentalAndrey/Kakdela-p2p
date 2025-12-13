893f56da5549e9817a156ac91ecaeeaa5ed42e3f
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-13 15:58:02 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,10 +6,16 @@ pluginManagement {
     }
 
     plugins {
-        id("com.android.application") version "8.4.0"
-        id("org.jetbrains.kotlin.android") version "2.2.21"
-        id("org.jetbrains.kotlin.plugin.compose") version "2.2.21"
-        id("com.google.devtools.ksp") version "2.2.21-1.0.21"
+        // Android Gradle Plugin
+        id("com.android.application") version "8.4.2"
+        id("com.android.library") version "8.4.2"
+
+        // Kotlin
+        id("org.jetbrains.kotlin.android") version "1.9.25"
+        id("org.jetbrains.kotlin.plugin.compose") version "1.9.25"
+
+        // ✅ ЕДИНСТВЕННАЯ РАБОЧАЯ ВЕРСИЯ KSP
+        id("com.google.devtools.ksp") version "1.9.25-1.0.26"
     }
 }
 
@@ -23,4 +29,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "Kakdela-p2p"
 include(":app")
-include(":goterl")
