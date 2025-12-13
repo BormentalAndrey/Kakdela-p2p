420eb45fa92b083a30234b3c570321c8af4a7a6a
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-13 10:05:32 +0300

    Update NavGraph.kt

--- app/src/main/java/com/kakdela/p2p/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/kakdela/p2p/ui/navigation/NavGraph.kt
@@ -11,20 +11,20 @@ import com.kakdela.p2p.ui.screens.QrScannerScreen
 
 @Composable
 fun NavGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = "contacts") {
-        composable("contacts") {
+    NavHost(navController = navController, startDestination = Screen.Contacts.route) {
+        composable(Screen.Contacts.route) {
             ContactsScreen(onOpenChat = { peerId ->
-                navController.navigate("chat/$peerId")
+                navController.navigate(Screen.Chat.route(peerId))
             })
         }
-        composable("chat/{peerId}") { backStackEntry ->
+        composable(Screen.Chat.route) { backStackEntry ->
             val peerId = backStackEntry.arguments?.getString("peerId") ?: ""
             ChatScreen(peerId = peerId, onBack = { navController.popBackStack() })
         }
-        composable("myQr") {
+        composable(Screen.MyQr.route) {
             MyQrScreen()
         }
-        composable("qrScanner") {
+        composable(Screen.QrScanner.route) {
             QrScannerScreen()
         }
     }
