04e6601d02ee3402e5802af26097de52d0ebaf44
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-13 06:48:31 +0300

    Update AppDatabase.kt

--- app/src/main/java/com/kakdela/p2p/db/AppDatabase.kt
+++ app/src/main/java/com/kakdela/p2p/db/AppDatabase.kt
@@ -15,15 +15,15 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun messageDao(): ChatMessageDao
 
     companion object {
-        @Volatile private var INSTANCE: AppDatabase? = null
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
 
-        fun getInstance(context: Context): AppDatabase =
-            INSTANCE ?: synchronized(this) {
-                INSTANCE ?: Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java,
-                    "kakdela.db"
-                ).build().also { INSTANCE = it }
-            }
+        fun getInstance(context: Context): AppDatabase = INSTANCE ?: synchronized(this) {
+            INSTANCE ?: Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "kakdela.db"
+            ).build().also { INSTANCE = it }
+        }
     }
 }
