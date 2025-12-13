2d5377259824f9116b4c9016e9a4e6a1a05f5f13
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-13 10:01:10 +0300

    Update Message.kt

--- app/src/main/java/com/kakdela/p2p/ui/model/Message.kt
+++ app/src/main/java/com/kakdela/p2p/ui/model/Message.kt
@@ -1,15 +1,4 @@
-// Message.kt
-package com.kakdela.p2p.data
+package com.kakdela.p2p.ui.model
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "messages")
-data class Message(
-    @PrimaryKey(autoGenerate = true) val id: Long = 0,
-    val chatId: String,
-    val senderId: String,
-    val author: String,
-    val text: String,
-    val timestamp: Long
-)
+// Используем db.ChatMessageEntity как Message
+typealias Message = com.kakdela.p2p.db.ChatMessageEntity
