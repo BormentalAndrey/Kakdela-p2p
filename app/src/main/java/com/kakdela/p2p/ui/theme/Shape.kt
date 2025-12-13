36529dcb75303ced2c993a19c0928beeecdd1987
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-13 10:13:02 +0300

    Update Shape.kt

--- app/src/main/java/com/kakdela/p2p/ui/theme/Shape.kt
+++ app/src/main/java/com/kakdela/p2p/ui/theme/Shape.kt
@@ -1,5 +1,9 @@
 package com.kakdela.p2p.ui.theme
 
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
-
-val Shapes = Shapes()
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
