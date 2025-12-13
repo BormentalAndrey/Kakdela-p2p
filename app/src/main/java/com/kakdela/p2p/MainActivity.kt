0b51bee066c48a9ec7744711c9fa425926ef4a41
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-13 06:45:57 +0300

    Update MainActivity.kt

--- app/src/main/java/com/kakdela/p2p/MainActivity.kt
+++ app/src/main/java/com/kakdela/p2p/MainActivity.kt
@@ -3,7 +3,6 @@ package com.kakdela.p2p
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.kakdela.p2p.ui.navigation.NavGraph
@@ -12,7 +11,6 @@ import com.kakdela.p2p.ui.theme.Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        App.instance = application as App
         setContent {
             Theme {
                 val navController = rememberNavController()
